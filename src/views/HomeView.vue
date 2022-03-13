49fb642439bb560715552a901961246df6f36c7d
mirko-san <41379069+mirko-san@users.noreply.github.com>
2022-03-13 21:27:31 +0900

    Add: newt のAPIレスポンスを用いた Home 画面

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,42 @@
 <template>
   <div class="home">
-    <h1>This is a home page</h1>
+    <section>
+      <v-parallax
+        height="300"
+        :src="core.heroImage.src"
+        class="d-flex align-center"
+      >
+        <v-row align="center" justify="center">
+          <v-col class="text-center" cols="12">
+            <h1 class="text-h4 pt-4">{{ core.siteName }}</h1>
+            <h4 class="pt-4">{{ core.siteDescription }}</h4>
+          </v-col>
+        </v-row>
+      </v-parallax>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import { Client } from "@/core/client";
+import { Core } from "@/mock/types/core";
 
 export default defineComponent({
   name: "HomeView",
+  data() {
+    return {
+      core: {} as Core,
+    };
+  },
   created() {
     this.get();
   },
   methods: {
     async get() {
       const client = new Client();
-      const res = await client.get("");
-      console.log(res);
+      const res = await client.get("core");
+      this.core = res.items[0] as Core;
     },
   },
 });
