3547c7326125d98d26af88ad4e7e69972bb42938
mirko-san <41379069+mirko-san@users.noreply.github.com>
2022-04-09 15:44:03 +0900

    Modify: Vuetify のカラーパレットに仮で色入れた

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,5 +5,22 @@ import "vuetify/styles";
 // Vuetify
 import { createVuetify } from "vuetify";
 
-export default createVuetify();
+export default createVuetify({
+  theme: {
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: "#7414A0",
+          secondary: "#424242",
+          accent: "#FA9B1E",
+          error: "#E7556E",
+          info: "#B92CA9",
+          success: "#35C5C7",
+          warning: "#F8CB5C",
+        },
+      },
+    },
+  },
+});
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
