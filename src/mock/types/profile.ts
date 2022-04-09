73cbb74d057c4ad80e0e3acb6ccb3d591ecc3bea
mirko-san <41379069+mirko-san@users.noreply.github.com>
2022-04-09 17:51:10 +0900

    Modify: Profile の型の修正

--- src/mock/types/profile.ts
+++ src/mock/types/profile.ts
@@ -1,8 +1,13 @@
 export interface Profile {
   name: string;
+  iconImage: {
+    _id?: string;
+    src: string;
+  };
   email: string;
   detail: string;
   twitterUrl: string;
   instagramUrl: string;
+  pixivUrl: string;
   scheduleText: string;
 }
