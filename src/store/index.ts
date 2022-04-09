9c75c94ac035f2907c294295184d9a1fe806139d
mirko-san <41379069+mirko-san@users.noreply.github.com>
2022-04-09 17:41:43 +0900

    Modify: Vuex の使い方を公式参考に TypeScript 用に修正

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,23 @@
-import { createStore } from "vuex";
+import { InjectionKey } from "vue";
+import { createStore, Store } from "vuex";
+import { works } from "./works";
 
-export default createStore({
-  state: {},
+interface State {
+  count: number;
+}
+
+// 公式の Composition API で Vue コンポーネントを記述する際の案内に従って key を定義
+// https://vuex.vuejs.org/ja/guide/typescript-support.html#usestore-%E9%96%A2%E6%95%B0%E3%81%AE%E5%9E%8B%E4%BB%98%E3%81%91
+export const key: InjectionKey<Store<State>> = Symbol();
+
+export const store = createStore({
+  state: {
+    count: 0,
+  },
   getters: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    works,
+  },
 });
