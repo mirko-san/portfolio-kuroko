8977a428acb671cd0f63b8e47399a46d853a5312
mirko-san <41379069+mirko-san@users.noreply.github.com>
2022-04-09 17:32:12 +0900

    Add: 共通中ヘッダーコンポーネントを追加

---
+++ src/components/common/CommonHeading02.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card variant="outlined" color="primary">
+    <v-card-title>
+      <h3>
+        <slot />
+      </h3>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
