25f36923e816f3cd8ce8df0047a8db61afe9d09f
mirko-san <41379069+mirko-san@users.noreply.github.com>
2022-04-09 15:50:35 +0900

    Add: 共通ヘッダーコンポーネントを追加

---
+++ src/components/common/CommonHeading.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card color="accent">
+    <v-card-title>
+      <h2>
+        <slot />
+      </h2>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
