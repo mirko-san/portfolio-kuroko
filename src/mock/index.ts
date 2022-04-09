ed021830a2c62a42bed1698330efe1b8711bd1f1
mirko-san <41379069+mirko-san@users.noreply.github.com>
2022-04-09 17:48:28 +0900

    Add: work の mock

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,23 +1,64 @@
 import fetchMock from "fetch-mock";
 
 import { Profile } from "./types/profile";
+import { Work } from "./types/work";
 
 const profile: Profile = {
   name: "name",
+  iconImage: {
+    src: "https://via.placeholder.com/500",
+  },
   email: "exampel.com",
   detail: "detail",
   twitterUrl: "https://twitter.com/example",
-  instagramUrl: "https://instagram.com/example",
+  instagramUrl: "https://www.instagram.com/example",
+  pixivUrl: "https://www.pixiv.net/users/example",
   scheduleText: "scheduleText",
 };
+
+const works: Work[] = [
+  {
+    title: "example title",
+    date: "2019-01-01T00:00:00.000Z",
+    image: {
+      src: "https://via.placeholder.com/1500",
+    },
+    thumbnailImage: {
+      src: "https://via.placeholder.com/500",
+    },
+  },
+  {
+    title: "example title",
+    date: "2020-01-01T00:00:00.000Z",
+    image: {
+      src: "https://via.placeholder.com/1500",
+    },
+    thumbnailImage: {
+      src: "https://via.placeholder.com/500",
+    },
+    url: "https://example.com/example",
+  },
+];
 
 export const init = () => {
   fetchMock.config.overwriteRoutes = false;
   fetchMock.config.fallbackToNetwork = true;
-  fetchMock.get("express:/profile", {
+  fetchMock.get("end:profile", {
     status: 200,
     body: {
-      ...profile,
+      skip: 0,
+      limit: 100,
+      total: 1,
+      items: [profile],
+    },
+  });
+  fetchMock.get("end:work", {
+    status: 200,
+    body: {
+      skip: 0,
+      limit: 2,
+      total: 4,
+      items: works,
     },
   });
 };
