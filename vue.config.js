4859c90c08d376c67858951b8c1ea5a4b18c57ec
mirko-san <41379069+mirko-san@users.noreply.github.com>
2023-03-18 13:20:49 +0900

    :up: 依存関係のアップデート

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
+const { VuetifyPlugin } = require("webpack-plugin-vuetify");
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
   pluginOptions: {
-    vuetify: {
-      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-    },
+    vuetify: [new VuetifyPlugin({ autoImport: true })],
   },
 });
